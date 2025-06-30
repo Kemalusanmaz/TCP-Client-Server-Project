63682864457060e89032471a11b7d1971be864ad
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2025-06-30 16:02:10 +0300

    gitignore file is added

--- client/src/clientMain.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/clientObject.hpp"
-
-int main() {
-  ClientObject clientObj;
-  clientObj.initialize();
-  clientObj.receiveMessage();
-  return 0;
-}
--- server/src/serverMain.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/serverObject.hpp"
-
-int main() {
-  ServerObject serverObj;
-  serverObj.initialize();
-  serverObj.transmitMessage();
-  return 0;
-}
